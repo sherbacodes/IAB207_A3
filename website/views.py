06fb6a7e572bde607511aea0b12d2216f12390e6
Hayden Seng <hseng1305@gmail.com>
2025-05-20 19:58:31 +1000

    Include category on search also

--- website/views.py
+++ website/views.py
@@ -1,38 +1,40 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user
 from sqlalchemy import or_
-from .models import Event
+from .models import Event, Category
 from .forms import CommentForm
 from . import db
 
 main_bp = Blueprint('main', __name__)
 
-# Homepage Route
 @main_bp.route('/')
 def index():
     events = db.session.scalars(db.select(Event)).all()
     return render_template('index.html', events=events)
 
-# Search Route
 @main_bp.route('/search')
 def search():
     search_term = request.args.get('search', '').strip()
     if search_term:
         query = f"%{search_term}%"
-        events = db.session.scalars(
-            db.select(Event).where(
+        # Join with Category to search category name
+        stmt = (
+            db.select(Event)
+            .join(Category)
+            .where(
                 or_(
                     Event.event_name.ilike(query),
                     Event.event_location.ilike(query),
-                    Event.event_description.ilike(query)
+                    Event.event_description.ilike(query),
+                    Category.name.ilike(query)  # <-- search category name here
                 )
             )
-        ).all()
+        )
+        events = db.session.scalars(stmt).all()
         return render_template('index.html', events=events)
     else:
         return redirect(url_for('main.index'))
 
-# Event Detail Page
 @main_bp.route('/event/<int:event_id>')
 def event_detail(event_id):
     event = db.get_or_404(Event, event_id)
